4d56772d621490d3fea180cba2b26896d9ffe1ba
Aleks Magnusson <92426232+aleksmagnusson@users.noreply.github.com>
2022-03-20 18:17:00 +0100

    ProductDetails Done, finally!

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,8 +1,17 @@
 // useRecoilValue används när man vill returnera ett värde som är givet till Recoil state.
 import { useRecoilValue, useRecoilState } from "recoil";
+import { Link } from "react-router-dom";
 // Hämtar in ProductsAtom från productsState.
 import { productsState } from "../components/recoil/products/productsAtom";
-import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Stack,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import { cartState } from "../components/recoil/cart/atom";
 import { useParams } from "react-router-dom";
 import React, { useState } from "react";
@@ -18,24 +27,52 @@ function ProductDetail() {
 
   // Hämta enskild produkt från Products till "ProductDetail".
   const product = products.find(
-    (product) => product.id === params.productId[0]
+    (product) => product.id === parseInt(params.productId, 10)
   );
 
   return (
-    <Box key={product.id}>
-      <Heading fontSize="larger">{product.title}</Heading>
-      <Text fontWeight="bold">{product.price} $</Text>
-      <Text fontStyle="italic">{product.description}</Text>
-      <Text fontWeight="medium">{product.category}</Text>
-      <Text fontWeight="bold">
-        {" "}
-        Rating: {product.rating.rate} of {product.rating.count} ratings.
-      </Text>
-      <br />
-      <Button key={product.id} onClick={() => handleAdd(product)}>
-        {" "}
-        Add product{" "}
-      </Button>
+    <Box minHeight="60vh">
+      <Stack spacing="50px">
+        <SimpleGrid columns={2} gridColumn="2">
+          <Box key={product.id} margin="auto">
+            <Image
+              display="block"
+              marginLeft="auto"
+              marginRight="auto"
+              width="25%"
+              src={product.image}
+            />
+            <Heading fontSize="larger" textAlign="center">
+              {product.title}
+            </Heading>
+            <Text fontWeight="bold" textAlign="center">
+              {product.price} $
+            </Text>
+            <Text fontStyle="italic" textAlign="center">
+              {product.description}
+            </Text>
+            <Text fontWeight="medium" textAlign="center">
+              {product.category}
+            </Text>
+            <Text fontWeight="bold" textAlign="center" left="50%">
+              {" "}
+              Rating: {product.rating.rate} of {product.rating.count} ratings.
+            </Text>
+            <br />
+            <Button marginRight="25" left="50%" as={Link} to="/products">
+              Go back
+            </Button>
+            <Button
+              left="50%"
+              key={product.id}
+              onClick={() => handleAdd(product)}
+            >
+              {" "}
+              Add product{" "}
+            </Button>
+          </Box>
+        </SimpleGrid>
+      </Stack>
     </Box>
   );
 }
